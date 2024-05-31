4ab9d685e78ee4e61ddb5d6f60879f86deb88e72
Максим Федоров <m88.fedorov@gmail.com>
2024-05-31 17:46:57 +0300

    Добавьте файлы проекта.

---
+++ Homework-29.05.2024/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Symbol: ");
+        string? symbol = Console.ReadLine();
+        Console.WriteLine("Enter quantity steps: ");
+        int steps = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < steps; i++)
+        {
+            Console.WriteLine(new string(' ', i * 2) + symbol + symbol + symbol);
+
+
+            if (i < steps)
+            {
+
+                Console.WriteLine(new string(' ', i * 2) + "  " + symbol);
+            }
+        }
+    }
+}
+
+
+
